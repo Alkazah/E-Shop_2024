d89a20a76c9c306c9eb10eb59b794a29245ae3b0
alkazah <alkazah38@gmail.com>
2024-04-27 20:12:21 +0200

    :)

---
+++ src/Artikel.java
@@ -0,0 +1,52 @@
+import javax.swing.* ;
+
+public class Artikel {
+
+    public static void main(String[] args) {
+
+    }
+
+    int artikelnummer;
+    int bestand;
+    String bezeichnung;
+    float preis;
+
+    public Artikel(int artikelnummer, int bestand, String bezeichnung, float preis) {
+        this.artikelnummer = artikelnummer;
+        this.bestand = bestand;
+        this.bezeichnung = bezeichnung;
+        this.preis = preis;
+    }
+
+    public int getArtikelnummer() {
+        return artikelnummer;
+    }
+
+    public void setArtikelnummer(int artikelnummer) {
+        this.artikelnummer = artikelnummer;
+    }
+
+    public int getBestand() {
+        return bestand;
+    }
+
+    public void setBestand(int bestand) {
+        this.bestand = bestand;
+    }
+
+    public String getBezeichnung() {
+        return bezeichnung;
+    }
+
+    public void setBezeichnung(String bezeichnung) {
+        this.bezeichnung = bezeichnung;
+    }
+
+    public float getPreis() {
+        return preis;
+    }
+
+    public void setPreis(float preis) {
+        this.preis = preis;
+    }
+}
