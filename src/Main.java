d6955f1a415054fbf26d5cb98e75800f1627c261
Ferhad Sabbagh <96669449+FerhadSabbagh@users.noreply.github.com>
2024-05-04 17:12:37 +0200

    Warenkorb Datenmodell

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,13 @@ import Verwaltungen.NutzerVerwaltung;
 import entities.Artikel;
 import entities.Kunde;
 import entities.Nutzer;
+import entities.Warenkorb;
 
 public class Main {
 
     public static void main(String[] args) {
         Artikel Kartoffel = new Artikel(1, 2, "Kartoffel", 12.50);
+        Artikel Pandora = new Artikel(1, 5, "Pandora", 75.00);
         Kunde kunde1 = new Kunde(1, "Haaa","ASsdsad", "asdasdas", "Strasse", "28256", "Bremen");
         System.out.println(kunde1.getName());
         System.out.println(Kartoffel);
@@ -19,12 +21,22 @@ public class Main {
         System.out.println(nutzerVerwaltung.anmelden("ahmad.com", "123"));
         System.out.println(nutzerVerwaltung.anmelden("ahmad2.com", "123"));
 
-        Nutzer nutzer = nutzerVerwaltung.anmelden("ahmad2.com", "123");
+        Nutzer nutzer = nutzerVerwaltung.anmelden("ahmad.com", "123");
         if (nutzer instanceof Kunde) {
             Kunde kunde = (Kunde) nutzer;
             System.out.println(kunde.getStrasse());
         } else {
             System.out.println("Angegebener Nutzer ist kein Kunde.");
         }
+
+        System.out.println("###############################");
+        Warenkorb warenkorb = new Warenkorb(nutzer);
+        System.out.println(warenkorb.getBesitzer().getName());
+        warenkorb.artikelHinzufuegen(Kartoffel, 2);
+        warenkorb.artikelHinzufuegen(Pandora, 3);
+        System.out.println(warenkorb.getArtikel());
+        warenkorb.artikelEntfernen(Pandora, 1);
+        System.out.println(warenkorb.getArtikel());
+        System.out.println(warenkorb);
     }
 }
--- src/entities/Warenkorb.java
+++ src/entities/Warenkorb.java
@@ -4,39 +4,42 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Warenkorb {
-    private Map<Artikel, Integer> artikelImWarenkorb;
-    private String artikelBezeichnung = "";
-    private int artikelAnzahl = 0;
-    private double gesamtPreis = 0;
+    private Nutzer besitzer;  // Der Nutzer, dem der Warenkorb gehört
+    private Map<Artikel, Integer> artikel; // Speichert Artikel und deren Mengen
 
-    public Warenkorb(String artikelBezeichnung, int artikelAnzahl, int gesamtPreis){
-        this.artikelBezeichnung = artikelBezeichnung;
-        this.artikelAnzahl = artikelAnzahl;
-        this.gesamtPreis = gesamtPreis;
+    public Warenkorb(Nutzer besitzer) {
+        this.besitzer = besitzer;
+        this.artikel = new HashMap<>();
     }
 
-
-    public void setArtikelBezeichnung(String artikelBezeichnung) {
-        this.artikelBezeichnung = artikelBezeichnung;
+    public void artikelHinzufuegen(Artikel artikel, int menge) {
+        this.artikel.put(artikel, this.artikel.getOrDefault(artikel, 0) + menge);
     }
 
-    public String getArtikelBezeichnung() {
-        return artikelBezeichnung;
+    public void artikelEntfernen(Artikel artikel, int menge) {
+        int aktuelleMenge = this.artikel.getOrDefault(artikel, 0);
+        if (aktuelleMenge <= menge) {
+            this.artikel.remove(artikel);
+        } else {
+            this.artikel.put(artikel, aktuelleMenge - menge);
+        }
     }
 
-    public void setArtikelAnzahl(int artikelAnzahl) {
-        this.artikelAnzahl = artikelAnzahl;
+    public Map<Artikel, Integer> getArtikel() {
+        return artikel;
     }
 
-    public int getArtikelAnzahl() {
-        return artikelAnzahl;
+    public Nutzer getBesitzer() {
+        return besitzer;
     }
 
-    public void setGesamtPreis(double gesamtPreis) {
-        this.gesamtPreis = gesamtPreis;
-    }
-
-    public double getGesamtPreis() {
-        return gesamtPreis;
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("Warenkorb von " + besitzer.getName() + ":\n");
+        for (Map.Entry<Artikel, Integer> entry : artikel.entrySet()) {
+            sb.append(entry.getKey().getBezeichnung()).append(" - Menge: ").append(entry.getValue()).append("\n");
+        }
+        return sb.toString();
     }
 }
+
