9e044897272eca4dbfb87b387d7cc3097af36cd1
Ferhad Sabbagh <96669449+FerhadSabbagh@users.noreply.github.com>
2024-05-06 21:16:14 +0200

    RechnungsVerwaltung add

---
+++ src/Verwaltungen/RechnungsVerwaltung.java
@@ -0,0 +1,54 @@
+package Verwaltungen;
+
+import entities.Artikel;
+import entities.Kunde;
+import entities.Rechnung;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class RechnungsVerwaltung {
+    private List<Rechnung> rechnungen;
+
+    public RechnungsVerwaltung() {
+        this.rechnungen = new ArrayList<>();
+    }
+
+    // Erstellt eine neue Rechnung und fügt sie der Liste der Rechnungen hinzu
+    public void rechnungErstellen(int bestellNr, Kunde kunde, Date datum, List<Artikel> gekaufteArtikel, double gesamtpreis) {
+        Rechnung neueRechnung = new Rechnung(bestellNr, kunde, datum, gekaufteArtikel, gesamtpreis);
+        rechnungen.add(neueRechnung);
+        System.out.println("Neue Rechnung erstellt: " + neueRechnung);
+    }
+
+    // Gibt alle Rechnungen zurück
+    public List<Rechnung> getAlleRechnungen() {
+        return new ArrayList<>(rechnungen);
+    }
+
+    // Gibt alle Rechnungen für einen bestimmten Kunden zurück
+    public List<Rechnung> getRechnungenFuerKunden(Kunde kunde) {
+        return rechnungen.stream()
+                .filter(rechnung -> rechnung.getKunde().equals(kunde))
+                .collect(Collectors.toList());
+    }
+
+    // Gibt Rechnungen basierend auf dem Datum zurück
+    public List<Rechnung> getRechnungenNachDatum(Date datum) {
+        return rechnungen.stream()
+                .filter(rechnung -> rechnung.getDatum().equals(datum))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("Alle Rechnungen:\n");
+        for (Rechnung rechnung : rechnungen) {
+            sb.append(rechnung).append("\n");
+        }
+        return sb.toString();
+    }
+}
+
--- src/Verwaltungen/Rechnungsverwaltung.java
+++
@@ -1,4 +0,0 @@
-package Verwaltungen;
-
-public class Rechnungsverwaltung {
-}
